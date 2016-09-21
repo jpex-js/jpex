ddee0a3e02f95548108aadf024a22622fe4d4a58
Jack <jack.m.ellis@hotmail.co.uk>
2016-09-21 14:21:39 +0100

    Travis

---
+++ lcov-fix.js
@@ -0,0 +1,16 @@
+var Jpex = require('./jpex');
+var App = Jpex.extend(function($fs, path){
+  var base = path.resolve('.') + path.sep;
+  $fs.readFile('./jpex/coverage/lcov.info', 'utf8')
+    .then(function(data){
+      while (data.indexOf(base) > -1){
+        data = data.replace(base, '');
+      }
+      return data.replace(/\\/g, '/');;
+    })
+    .then(function(data){
+      return $fs.writeFile('./lcov.info', data, 'utf8');
+    });
+});
+
+new App();
