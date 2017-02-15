52cf1e8e7b487d97f09c4909f804568a666774d8
Jack Ellis <Jack@Administrators-MacBook-Pro.local>
2017-02-15 22:49:47 +0000

    .

--- lcov-fix.js
+++ lcov-fix.js
@@ -1,7 +1,7 @@
 var Jpex = require('./jpex');
 var App = Jpex.extend(function($fs, path){
   var base = path.resolve('.') + path.sep;
-  $fs.readFile('./jpex/coverage/lcov.info', 'utf8')
+  $fs.readFile('./coverage/lcov.info', 'utf8')
     .then(function(data){
       while (data.indexOf(base) > -1){
         data = data.replace(base, '');
