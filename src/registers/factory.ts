8e703a86913f081962e526c4d20c69e59950e516
jackmellis <jack.m.ellis@hotmail.co.uk>
2021-03-05 10:35:33 +0000

    refactor: shortcust constant
    registering a constant should just create an already-resolved entry
    this will save us an unecessary trip through the resolve code for all constants
    
    closes #110

--- src/registers/factory.ts
+++ src/registers/factory.ts
@@ -1,17 +1,5 @@
-import { JpexInstance, Factory, Dependency, AnyFunction, FactoryOpts } from '../types';
-import { isString, isFunction, hasLength, ensureArray } from '../utils';
-
-export const validateArgs = (name: string, dependencies: Dependency[], fn: AnyFunction) => {
-  if (!isString(name)) {
-    throw new Error(`Factories must be given a name, but was called with [${typeof name}]`);
-  }
-  if (!Array.isArray(dependencies)) {
-    throw new Error(`Expected an array of dependencies, but was called with [${typeof dependencies}]`);
-  }
-  if (!isFunction(fn)) {
-    throw new Error(`Factory ${name} must be a [Function]`);
-  }
-};
+import { JpexInstance, Dependency, AnyFunction, FactoryOpts } from '../types';
+import { hasLength, ensureArray, isPassive, validateArgs } from '../utils';
 
 export default function factory <T>(
   this: JpexInstance,
@@ -26,17 +14,15 @@ export default function factory <T>(
     dependencies = null;
   }
 
-  // eslint-disable-next-line max-len
-  if ((opts.precedence || this.$$config.precedence) === 'passive' && this.$$factories[name] != null) {
+  if (isPassive(name, this, opts.precedence)) {
     return;
   }
 
-  const f: Factory = {
+  this.$$factories[name] = {
     fn,
     dependencies,
     lifecycle: opts.lifecycle || this.$$config.lifecycle,
   };
-  this.$$factories[name] = f;
 
   if (opts.alias) {
     ensureArray(opts.alias).forEach(alias => this.alias(alias, name));
